e24a4065026c65694f258ddba9893670f098ebdc
Prashant Dewangan <166482484+prashant030399@users.noreply.github.com>
2024-08-09 12:02:48 +0530

    Add files via upload

---
+++ routemultiplefileinsert/select.js
@@ -0,0 +1,35 @@
+
+
+
+
+var express = require('express');
+var  mysql = require('mysql2');
+
+var router = express.Router();
+
+router.get('/', function(req, res){
+   
+
+var con = mysql.createConnection({
+   host: "localhost",
+   user: "root",
+   password: "seedit",
+   database: "nodestudent"
+ });
+ 
+ 
+ con.connect(function(err) {
+   if (err) throw err;
+   console.log("Connected!");
+   var sql = "select * from info";
+   con.query(sql, function (err, result) {
+     if (err) throw err;
+     res.send(JSON.stringify(result));
+   });
+ });
+});
+
+
+
+//export this router to use in our index.js
+module.exports = router;
